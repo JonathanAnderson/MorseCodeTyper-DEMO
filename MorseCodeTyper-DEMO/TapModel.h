1de7ca0ce1d4c325d22da8d6f3a373caea771492
Jonathan Anderson <jonathan.anderson@gmail.com>
2011-12-07 22:41:19 -0600

    first commit

---
+++ MorseCodeTyper-DEMO/TapModel.h
@@ -0,0 +1,13 @@
+//
+//  TapModel.h
+//  MorseCodeTyper-DEMO
+//
+//  Created by Jonathan Anderson on 12/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TapModel : NSObject
+
+@end
